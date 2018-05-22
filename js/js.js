7fb37f5e9ea32cd6b70eccac2c32e7fcef651a96
quentin mccarthy <845868401@bun2-206-14.local>
2018-05-23 09:08:01 +1200

    Finished layout; added pseudocode

---
+++ js/js.js
@@ -0,0 +1,14 @@
+// Pseudocode
+// Initialise an object with all the relevant variables and methods
+// Some variables and methods the object will contain are:
+// An empty array
+// A write method which will toggle between allowing users to write in the input
+// And sending the input data into the array
+// A delete method
+// Which will remove the item from the DOM and array
+// A tick method
+// Which will toggle the tick icon between tick and unticked
+// Inside a document ready will be three click events
+// One for the writeButton id that fires the write method in the object
+// One for the delButton id that fires the delete method in the object
+// One for the tickBox class that fires the tick method in the object
